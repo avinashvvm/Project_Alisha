cd4c194eb23272cf828ab2ce8870ebb83b7dca2e
Aviansh <avinashvvm@gmail.com>
2018-01-18 11:01:45 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,11 @@
-Sudo -i
 apt-get install python
 python -m pip install -U pip
 pip install pyttsx
 pip install SpeechRecognition
+sudo apt-get install portaudio19-dev
+sudo apt-get install python-pyaudio
+sudo apt-get -y install python3-pip
+sudo apt-get update
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+pip3 --version 
